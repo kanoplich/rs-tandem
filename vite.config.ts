09b4cd623369828e4796a9ec64b1deafbcc307e8
Andrei Kanoplich <73616099+kanoplich@users.noreply.github.com>
2026-02-14 19:03:55 +0300

    Team42 5/chore/config (#1)
    * chore: add config
    
    * docs: add diary for 2026-02-14

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+/// <reference types="vitest" />
+import path from 'path';
+
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
+import { defineConfig } from 'vitest/config';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss(), svgr()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './vitest.setup.ts',
+  },
+});
