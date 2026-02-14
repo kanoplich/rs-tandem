6ad6627aeeaed422b9c5bd4d07cb0e7e5ffed9d7
Andrei Kanoplich <andrei.kanoplich@gmail.com>
2026-02-14 15:31:42 +0300

    chore: add config

---
+++ src/app/index.tsx
@@ -0,0 +1,12 @@
+import { Camera } from 'lucide-react';
+
+const App = () => {
+  return (
+    <>
+      <div>hello world</div>
+      <Camera color="red" size={48} />
+    </>
+  );
+};
+
+export default App;
