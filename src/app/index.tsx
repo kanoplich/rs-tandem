46566773457204c0c8c55d5f50ae53b427dc718a
Fatima <itxtoyif@gmail.com>
2026-02-18 15:55:30 +0600

    style: refactor global css and theme variables
    * style: refactor global css and theme variables
    
    * style: add tailwind docs folder

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,7 @@
-import { Camera } from 'lucide-react';
-
 const App = () => {
   return (
     <>
-      <div>hello world</div>
-      <Camera color="red" size={48} />
+      <h1>Start App</h1>
     </>
   );
 };
