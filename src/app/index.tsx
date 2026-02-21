f0a87fe9deed529d1c3a33541a231a0c3e5bb56c
Valer <rockabil81@gmail.com>
2026-02-21 22:07:16 +0300

    Team42 26/feat/router (#7)
    * feat: add header and footer file
    
    * chore: fill notebook's notation
    
    * fix: change implementation of router
    
    * feat: add export file in pages folder

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,7 @@
+import { RouterProvider } from 'react-router-dom';
+
+import { router } from './router';
+
 export const App = () => {
-  return (
-    <>
-      <h1>Start App</h1>
-    </>
-  );
+  return <RouterProvider router={router} />;
 };
