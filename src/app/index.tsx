74e1b9ba53b7dd6075018a94e0434be26228d8a0
rockabil <rockabil81@gmail.com>
2026-02-21 15:51:44 +0300

    fix: change implementation of router

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,14 +1,7 @@
 import { RouterProvider } from 'react-router-dom';
 
-import { AppLayout } from './layout';
 import { router } from './router';
 
 export const App = () => {
-  return (
-    <>
-      <h1>Start App</h1>
-      <AppLayout />
-      <RouterProvider router={router} />
-    </>
-  );
+  return <RouterProvider router={router} />;
 };
--- src/app/layout/index.tsx
+++ src/app/layout/index.tsx
@@ -1,12 +1,10 @@
 import { Outlet } from 'react-router-dom';
 
 import { Footer } from './footer';
-import { Header } from './header';
 
 export const AppLayout = () => {
   return (
     <>
-      <Header />
       <main>
         <Outlet />
       </main>
