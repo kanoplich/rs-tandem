33d2c4692cf0ce4dcc3b9499a19bd7284099fcce
Andrei Kanoplich <andrei.kanoplich@gmail.com>
2026-02-23 20:32:06 +0300

    fix: delete temporary functions

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,27 +1,7 @@
-import { useEffect } from 'react';
 import { RouterProvider } from 'react-router-dom';
 
 import { router } from './router';
 
-import { getSession, onAuthStateChange } from '@/shared/api';
-
 export const App = () => {
-  useEffect(() => {
-    const getInitialSession = async () => {
-      try {
-        await getSession();
-      } catch (error: unknown) {
-        console.error('Error loading session:', error);
-      }
-    };
-    getInitialSession();
-    const subscription = onAuthStateChange((_session) => {
-      console.log(`session changed`);
-    });
-    return () => {
-      subscription?.unsubscribe();
-    };
-  }, []);
-
   return <RouterProvider router={router} />;
 };
--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -1,20 +1,6 @@
-import { signUp } from '@/shared/api';
-import type { AuthCredentials } from '@/shared/api/auth/types';
-import { Button } from '@/shared/ui/button';
-
 export const Register = () => {
-  const handleRegistration = async () => {
-    const viewer: AuthCredentials = {
-      email: 'andrei.kanoplich@gmail.com',
-      password: 'QdfFDfdsfsdwerty',
-    };
-
-    await signUp({ ...viewer });
-  };
-
   return (
     <>
-      <Button onClick={handleRegistration}></Button>
       <div>register</div>
     </>
   );
