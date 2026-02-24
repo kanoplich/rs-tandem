7be012cd7d35daebfd60132d53a3d765554759f4
Andrei Kanoplich <73616099+kanoplich@users.noreply.github.com>
2026-02-24 21:36:52 +0300

    Team42 14/feat/supabase (#17)
    * feat: add supabase config
    
    * feat: add database types
    
    * chore: fix pnpm config
    
    * feat: add supabase session api
    
    * feat: add temporary functions for check registration
    
    * fix: delete temporary functions
    
    * fix: lint mistake
    
    * feat: add auth function
    
    * feat: add mock data

---
+++ src/shared/api/auth/index.ts
@@ -0,0 +1,105 @@
+import type { Session } from '@supabase/supabase-js';
+
+import { supabase } from '../supabase-client';
+
+import { addMockAuthListener, MOCK_SESSION, mockIsAuth, setMockIsAuth } from './mock';
+import type { AuthCredentials } from './types';
+
+import { ROUTES } from '@/shared/config/routes';
+import { config } from '@/shared/config/supabase';
+import { delay } from '@/shared/lib/delay';
+
+const { USE_MOCK_SUPABASE, HOST } = config;
+
+export const getSession = async (): Promise<Session | null> => {
+  if (USE_MOCK_SUPABASE) {
+    return mockIsAuth ? MOCK_SESSION : null;
+  }
+
+  const {
+    data: { session },
+    error,
+  } = await supabase.auth.getSession();
+
+  if (error) {
+    throw error;
+  }
+
+  return session;
+};
+
+export const onAuthStateChange = (callback: (session: Session | null) => void) => {
+  if (USE_MOCK_SUPABASE) {
+    callback(mockIsAuth ? MOCK_SESSION : null);
+    return addMockAuthListener(callback);
+  }
+
+  const {
+    data: { subscription },
+  } = supabase.auth.onAuthStateChange((_event, session) => {
+    callback(session);
+  });
+
+  return subscription;
+};
+
+export const signUp = async ({ email, password }: AuthCredentials): Promise<Session> => {
+  if (USE_MOCK_SUPABASE) {
+    await delay(400);
+    setMockIsAuth(true);
+    return MOCK_SESSION;
+  }
+
+  const {
+    data: { session },
+    error,
+  } = await supabase.auth.signUp({
+    email,
+    password,
+    options: {
+      emailRedirectTo: `${HOST}${ROUTES.DASHBOARD}`,
+    },
+  });
+
+  if (error || !session) {
+    throw error || new Error('SingUp error');
+  }
+
+  return session;
+};
+
+export const singIn = async ({ email, password }: AuthCredentials): Promise<Session> => {
+  if (USE_MOCK_SUPABASE) {
+    await delay(400);
+    setMockIsAuth(true);
+    return MOCK_SESSION;
+  }
+
+  const {
+    data: { session },
+    error,
+  } = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  });
+
+  if (error || !session) {
+    throw error || new Error('SingIn error');
+  }
+
+  return session;
+};
+
+export const singOut = async (): Promise<void> => {
+  if (USE_MOCK_SUPABASE) {
+    await delay(400);
+    setMockIsAuth(false);
+    return;
+  }
+
+  const { error } = await supabase.auth.signOut();
+
+  if (error) {
+    throw error;
+  }
+};
---
+++ src/shared/api/supabase-client.ts
@@ -0,0 +1,10 @@
+import { createClient } from '@supabase/supabase-js';
+
+import { config } from '@/shared/config/supabase';
+import type { Database } from '@/shared/types/database.types';
+
+export type Public = Database['public'];
+
+const { SUPABASE_URL, SUPABASE_ANON_KEY } = config;
+
+export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_ANON_KEY);
