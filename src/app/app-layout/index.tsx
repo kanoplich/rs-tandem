5500c9f1a1f4bfd39ea19cf9d85fc39ba882fa3a
Seva Kavalenka <seva.kavalenka@gmail.com>
2026-02-26 16:33:27 +0100

    feat: add sign in

--- src/app/app-layout/index.tsx
+++ src/app/app-layout/index.tsx
@@ -2,10 +2,20 @@ import { Outlet } from 'react-router-dom';
 
 import { Footer } from './footer';
 
-import { evaluateTheory } from '@/shared/api';
+import { evaluateTheory, signUp, singIn } from '@/shared/api';
 import { Button } from '@/shared/ui/button';
 
 export const AppLayout = () => {
+  const handleSignUp = async () => {
+    const session = await signUp({ email: 'seva.kavalenka@gmail.com', password: 'Qwerty12345' });
+    console.log('Signed up session:', session);
+  };
+
+  const handleSignIn = async () => {
+    const session = await singIn({ email: 'seva.kavalenka@gmail.com', password: 'Qwerty12345' });
+    console.log('Signed in session:', session);
+  };
+
   const handleGroqApi = async () => {
     const result = await evaluateTheory('123', 'Привет');
     console.log(result);
@@ -14,6 +24,8 @@ export const AppLayout = () => {
   return (
     <>
       <main>
+        <Button onClick={() => handleSignUp()}>SignUp</Button>
+        <Button onClick={() => handleSignIn()}>SignIn</Button>
         <Button onClick={() => handleGroqApi()}>Groq_API</Button>
         <Outlet />
       </main>
