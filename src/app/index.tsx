eec04de196f02ed02f0fc33cc522c06c65401638
rockabil <rockabil81@gmail.com>
2026-02-28 19:52:09 +0300

    feat: add authprovider in app file

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,12 @@
 import { RouterProvider } from 'react-router-dom';
 
+import { AuthProvider } from './providers/auth-provider';
 import { router } from './router';
 
 export const App = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <AuthProvider>
+      <RouterProvider router={router} />;
+    </AuthProvider>
+  );
 };
