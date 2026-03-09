08d524240efbb4187fc0ef8a8b3fcf54cbfea8ef
Bakasova FAtima <itxtoyif@gmail.com>
2026-03-09 21:39:56 +0600

    refactor: minor cleanup in header

--- src/app/public-layout/index.tsx
+++ src/app/public-layout/index.tsx
@@ -1,12 +1,10 @@
 import { Outlet } from 'react-router-dom';
 
 import { Footer } from '../ui/footer';
-import { Header } from '../ui/header';
 
 export const PublicLayout = () => {
   return (
     <>
-      <Header />
       <main>
         <Outlet />
       </main>
