d3436b1e72b22bc225aa3f8207175eb39edb5299
Andrei Kanoplich <andrei.kanoplich@gmail.com>
2026-03-14 12:52:58 +0300

    feat: add profile files

--- src/shared/api/auth/index.ts
+++ src/shared/api/auth/index.ts
@@ -3,7 +3,7 @@ import type { Provider } from '@supabase/supabase-js';
 import { supabase } from '../supabase-client';
 
 import { addMockAuthListener, MOCK_SESSION, mockIsAuth, setMockIsAuth } from './mock';
-import type { AuthCredentials, AuthSession, AuthUser, RegisterCredentials } from './types';
+import type { AuthCredentials, AuthSession, RegisterCredentials } from './types';
 
 import { ROUTES } from '@/shared/config/routes';
 import { config } from '@/shared/config/supabase';
@@ -132,19 +132,4 @@ export const resetPassword = async (email: string): Promise<void> => {
   });
 
   if (error) throw error;
-};
-
-export const updateUser = async (password: string): Promise<AuthUser | null> => {
-  const {
-    data: { user },
-    error,
-  } = await supabase.auth.updateUser({
-    password,
-  });
-
-  if (error || !user) {
-    throw error || new Error('Failed to update password');
-  }
-
-  return user;
 };
