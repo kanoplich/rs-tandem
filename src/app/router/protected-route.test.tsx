c8dca8224d5f69d76ca9a057ff24514596a75818
rockabil <rockabil81@gmail.com>
2026-03-15 17:54:32 +0300

    test: test protected-route file

---
+++ src/app/router/protected-route.test.tsx
@@ -0,0 +1,88 @@
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter, Route, Routes } from 'react-router-dom';
+import { describe, test, expect, vi } from 'vitest';
+
+import { ProtectedRoute } from './protected-route';
+
+import { ROUTES } from '@/shared/config/routes';
+import { useAuth } from '@/shared/hooks/use-auth';
+
+vi.mock('@/shared/hooks/use-auth', () => ({
+  useAuth: vi.fn(),
+}));
+
+vi.mock('@/app/ui/loader', () => ({
+  Loader: () => <div data-testid="loader">Loading...</div>,
+}));
+
+const mockUseAuth = vi.mocked(useAuth);
+
+const TestComponent = ({
+  children,
+  reverse = false,
+}: {
+  children: React.ReactNode;
+  reverse?: boolean;
+}) => (
+  <MemoryRouter initialEntries={['/test']}>
+    <Routes>
+      <Route path="/test" element={<ProtectedRoute reverse={reverse}>{children}</ProtectedRoute>} />
+      <Route path={ROUTES.LOGIN} element={<div data-testid="login-page" />} />
+      <Route path={ROUTES.DASHBOARD} element={<div data-testid="dashboard-page" />} />
+    </Routes>
+  </MemoryRouter>
+);
+
+describe('ProtectedRoute', () => {
+  test('Shows Loader, when isLoading = true', () => {
+    mockUseAuth.mockReturnValue({
+      isAuthenticated: false,
+      isLoading: true,
+      session: null,
+      user: null,
+    });
+
+    render(
+      <TestComponent>
+        <div data-testid="protected-content">Protected Content</div>
+      </TestComponent>
+    );
+
+    expect(screen.getByTestId('loader')).toBeInTheDocument();
+    expect(screen.queryByTestId('protected-content')).not.toBeInTheDocument();
+  });
+
+  test('Redirect to /login, if is not authenticated (reverse=false)', () => {
+    mockUseAuth.mockReturnValue({
+      isAuthenticated: false,
+      isLoading: false,
+      session: null,
+      user: null,
+    });
+
+    render(
+      <TestComponent>
+        <div data-testid="protected-content">Protected Content</div>
+      </TestComponent>
+    );
+    expect(screen.getByTestId('login-page')).toBeInTheDocument();
+    expect(screen.queryByTestId('protected-content')).not.toBeInTheDocument();
+  });
+
+  test('Show children, if is not athenticated and reverse=true', () => {
+    mockUseAuth.mockReturnValue({
+      isAuthenticated: false,
+      isLoading: false,
+      session: null,
+      user: null,
+    });
+
+    render(
+      <TestComponent reverse>
+        <div data-testid="protected-content">Protected Content</div>
+      </TestComponent>
+    );
+
+    expect(screen.getByTestId('protected-content')).toBeInTheDocument();
+  });
+});
