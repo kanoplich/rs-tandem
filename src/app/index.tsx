d67179560a9c56992cfd488fb15719d4ac506a4d
Fatima <itxtoyif@gmail.com>
2026-03-18 17:16:44 +0600

    Team42 132/Добавление компонента ErrorBoundary для приложения (#142)
    * feat: add error-boundary with styles
    
    * feat: improve error handling and UI

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,14 @@
 import { RouterProvider } from 'react-router-dom';
 
-import { AuthProvider } from './providers';
+import { AuthProvider, ErrorBoundary } from './providers';
 import { router } from './router';
 
 export const App = () => {
   return (
-    <AuthProvider>
-      <RouterProvider router={router} />
-    </AuthProvider>
+    <ErrorBoundary>
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
+    </ErrorBoundary>
   );
 };
