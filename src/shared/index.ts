ffe63d651ea397622e4de7e8a890ea82b62d444a
Olga Zhyzhka <helgazhyzhka@gmail.com>
2026-03-23 13:30:23 +0000

    Revert "feat: create hisory page & logic" (#184)
    This reverts commit 14323d634970e436ef0a2e655d0989e4b184c489.

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -7,4 +7,3 @@ export { PROVIDERS, DEFAULT_MAX_SCORE, STAGES, DEFAULT_STAGES_VALUE } from './li
 export { getProgressPercent } from './lib/get-progress-percent';
 export { isTopicCompleted } from './lib/is-topic-completed';
 export { formatScore } from './lib/format-score';
-export { groupByStage } from './lib/group-by-stage';
