44bf0ce60ba45639a11a91b1982916191c630363
Seva Kavalenka <seva.kavalenka@gmail.com>
2026-03-26 13:00:32 +0100

    Team42-161/ Dashboard layout (#203)
    * feat: add dashboard-layout
    
    * fix: create StageCard and StatCard, fix components import, use STATS constants
    
    * fix: add types to components, fix import
    
    * fix: set stats as strings

---
+++ src/pages/dashboard/ui/overall-stats/index.tsx
@@ -0,0 +1,29 @@
+import { OVERALL_STATS_TEXT } from '../../locales';
+import { StatCard } from '../stat-card';
+
+import { ProgressIcon, StatisticIcon, TopicIcon } from '@/shared/assets/icons';
+
+export const OverallStats = () => {
+  return (
+    <section className="mb-8 bg-card border border-border rounded-xl">
+      <p className="pt-5 px-6 text-light">{OVERALL_STATS_TEXT.HEADER}</p>
+      <div className="p-6 grid grid-cols-1 sm:grid-cols-3 gap-6 ">
+        <StatCard
+          icon={<ProgressIcon />}
+          description={OVERALL_STATS_TEXT.GENERAL_PROGRESS_CARD_TEXT}
+          stats="42%"
+        />
+        <StatCard
+          icon={<TopicIcon />}
+          description={OVERALL_STATS_TEXT.AVERAGE_SCORE_CARD_TEXT}
+          stats="6.5/10"
+        />
+        <StatCard
+          icon={<StatisticIcon />}
+          description={OVERALL_STATS_TEXT.TOPICS_FINISHED_CARD_TEXT}
+          stats="16/37"
+        />
+      </div>
+    </section>
+  );
+};
---
+++ src/pages/dashboard/ui/start-button/index.tsx
@@ -0,0 +1,15 @@
+import { Link } from 'react-router-dom';
+
+import { START_BUTTON_TEXT } from '../../locales';
+
+import { Button, ROUTES } from '@/shared';
+
+export const StartButton = () => {
+  return (
+    <section className="mb-8 mt-4 sm:mb-4 flex justify-center">
+      <Button asChild className="w-full sm:w-auto px-9 py-7 text-lg">
+        <Link to={ROUTES.TOPICS}>{START_BUTTON_TEXT}</Link>
+      </Button>
+    </section>
+  );
+};
