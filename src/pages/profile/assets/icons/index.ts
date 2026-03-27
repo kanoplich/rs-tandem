576ccc6558464855a3b22b3301a5360e0b5b19ba
Fatima <itxtoyif@gmail.com>
2026-03-28 01:04:50 +0600

    Team42-168/Достижения пользователя (AchievementsCard) (#217)
    * feat: add achievements-card
    
    * refactor: simplify logic, remove unnecessary groupByStage
    
    * refactor: simplify achievements calculation and fix stageBadges check
    
    * feat: use getTopicStats for averageScore
    
    * refactor(stats-cards): replace hardcoded score with MAX_DISPLAY_SCORE

---
+++ src/pages/profile/assets/icons/index.ts
@@ -0,0 +1,5 @@
+export { default as FirstStep } from './first-step.svg?react';
+export { default as TenWorkouts } from './ten-workouts.svg?react';
+export { default as StageMaster } from './stage-master.svg?react';
+export { default as Perfection } from './perfection.svg?react';
+export { default as Expert } from './expert.svg?react';
