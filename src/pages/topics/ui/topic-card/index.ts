724764ff9a4716ea1d46bf2f8662c77b3156fcf5
rockabil <rockabil81@gmail.com>
2026-03-28 10:57:04 +0300

    feat: add topic folders

---
+++ src/pages/topics/ui/topic-card/index.ts
@@ -0,0 +1,8 @@
+import type { S } from 'node_modules/framer-motion/dist/types.d-CQ4vRM6h';
+
+interface TopicCardProps {
+  description: string;
+  stats: string;
+}
+
+export const TopicCard = () => {};
---
+++ src/pages/topics/ui/topic-grid/index.tsx
@@ -0,0 +1,7 @@
+export const TopicGrid = () => {
+  return (
+    <section className="mb-8 bg-card border border-border rounded-xl">
+      <div className="p-6 grid grid-cols-2 sm:grid-cols-1 gap-6"></div>
+    </section>
+  );
+};
