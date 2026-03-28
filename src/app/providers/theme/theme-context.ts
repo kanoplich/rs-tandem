938c48cdb3f8d6596aa6655a0c1dc9955dc3ba3b
vadim-troian <vadymtroiyan@gmail.com>
2026-03-28 17:00:17 +0200

    fix: delete not used type

--- src/app/providers/theme/theme-context.ts
+++ src/app/providers/theme/theme-context.ts
@@ -4,12 +4,6 @@ import { THEMES } from './locales';
 
 export type Theme = (typeof THEMES)[keyof typeof THEMES];
 
-type ThemeProviderProps = {
-  children: React.ReactNode;
-  defaultTheme?: Theme;
-  storageKey?: string;
-};
-
 type ThemeProviderState = {
   theme: Theme;
   setTheme: (theme: Theme) => void;
