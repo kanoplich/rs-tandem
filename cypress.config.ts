e82045952076b673bccb1aa16221ee2706f9c129
Seva Kavalenka <seva.kavalenka@gmail.com>
2026-04-03 15:37:50 +0200

    fix: remove unused code

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,9 +5,5 @@ export default defineConfig({
 
   e2e: {
     specPattern: 'cypress/e2e/**/*.ts',
-
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
   },
 });
