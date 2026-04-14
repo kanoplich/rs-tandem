2a14aa625e29a156582935c287df84d11ff8acdc
artkoro94 <artkoro94s@gmail.com>
2026-04-14 16:49:38 +0400

    fix: delete underline

--- src/features/auth/ui/forgot-form.tsx
+++ src/features/auth/ui/forgot-form.tsx
@@ -34,7 +34,7 @@ export const ForgotForm = () => {
         </Form>
 
         <p className="text-center text-sm text-muted-foreground">
-          <Link to={ROUTES.LOGIN} className="text-primary underline underline-offset-4">
+          <Link to={ROUTES.LOGIN} className="text-primary ">
             {FORGOT_PASSWORD_FORM_TEXT.LOGIN_LINK}
           </Link>
         </p>
