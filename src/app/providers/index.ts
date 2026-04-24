b7cc5dc778e888d855e66fcadd3acdc2ed446da2
HelgaZhizhka <helgazhyzhka@gmail.com>
2026-04-24 13:37:06 +0100

    fix: delete import

--- src/app/providers/index.ts
+++ src/app/providers/index.ts
@@ -1,4 +1,3 @@
 export { AuthProvider } from './auth-provider';
 export { ErrorBoundary } from './error-boundary';
-export { RouteErrorBoundary } from './error-boundary/route-error-boundary';
 export { ThemeProvider } from './theme/theme-provider';
